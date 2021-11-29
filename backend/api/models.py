3eeba915d03f8dbd10a4e9ffbdadf91ca8267c8e
Jose Jonathan Emmanuel <jonathan-emmanuel.jose@epitech.eu>
2021-11-29 12:36:29 +0100

    add some tables to make widgets work

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,30 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
-class Logins(models.Model):
+class WidgetsParam(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return "[" + self.name + ", " + self.type + "]"
+
+class Widgets(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=256)
+    params = models.ForeignKey(WidgetsParam, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class UserData(models.Model):
     email = models.EmailField(max_length=256)
     password = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    widgets = models.ForeignKey(to=Widgets, blank=True, on_delete=models.CASCADE)
     
-class UserData(models.Model):
-    logins = models.OneToOneField(
-        Logins,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
+    def __str__(self):
+        return self.first_name + " " + self.last_name
