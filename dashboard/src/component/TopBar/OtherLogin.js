3c31e49a5b3109c1fe31a801f63f6966438ef999
clara <yuhui.xu@epitech.eu>
2021-12-04 00:18:59 +0100

    Login with Google done

--- dashboard/src/component/TopBar/OtherLogin.js
+++ dashboard/src/component/TopBar/OtherLogin.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './TopBar.css';
+import { GoogleLogin } from 'react-google-login';
 
 export default function OtherLogin(props) {
+    const id = "132511071634-tredgcl1fgasbc8e9s30vb83i37lj199.apps.googleusercontent.com";
+
+    const onLoginSuccess = (res) => {
+        console.log("Login Success: ", res.profileObj);
+    }
+
+    const onLoginFail = (res) => {
+        console.log("FAIL: ", res)
+    }
+
     return (
         <div className="other-login">
-            <button className="other-inner">
-                <img
-                    src={require('./../../assets/google_logo.png').default}
-                    alt="google"
-                    className="other-logo"
-                />
-                Sign in with Google
-            </button>
-
-            <button className="other-inner">
-                <img
-                    src={require('./../../assets/twitter_logo.png').default}
-                    alt="twitter"
-                    className="other-logo"
-                />
-                Sign in with Twitter
-            </button>
+            <GoogleLogin
+                clientId={id}
+                buttonText="Login"
+                onSuccess={onLoginSuccess}
+                onFailure={onLoginFail}
+                cookiePolicy={'single_host_origin'}
+                className="other-inner"
+            />
         </div>
     )
 }
