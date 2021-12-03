af6f5ae016889ee4f49ea235d5a427873250c6a7
clara <yuhui.xu@epitech.eu>
2021-12-03 18:21:41 +0100

    edit profile ok

--- dashboard/src/component/TopBar/TopBar.jsx
+++ dashboard/src/component/TopBar/TopBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './TopBar.css';
 import Popup from '../Tools/Popup/Popup';
 import Signin from './Signin';
+import { Link } from "react-router-dom";
 import Subscribe from './Subscribe';
 
 export default function TopBar() {
@@ -15,6 +16,18 @@ export default function TopBar() {
                     <img src={require('./../../assets/logo.png').default} alt="Logo" width="40" height="40" margin-right="10px" />
                     <span className="logo">ClacBoard</span>
                 </div>
+                {global.in === true ? 
+                <div className="topRight">
+                    <Link to="/profile" style={{textDecoration: 'none'}}>
+                    <img
+                        src={require('../../assets/user.png').default}
+                        alt="profile_photo"
+                        className="Top-pp"
+                    />
+                    </Link>
+                    <label className="Top-Username">{global.username}</label>
+                </div>
+                :
                 <div className="topRight">
                     <button className="Up"
                     onClick={() => setBtnUp(true)}>
@@ -25,6 +38,7 @@ export default function TopBar() {
                         SIGN IN
                     </button>
                 </div>
+                }
             </div>
             <Popup trigger={btnUp} setTrigger={setBtnUp}>
                 <Subscribe trigger={btnUp} setTrigger={setBtnUp}/>
